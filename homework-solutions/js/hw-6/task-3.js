115e798af864b05eb8ed6c9a5c0e2bd4be7766dd
Rauf Rustam <raufonex@gmail.com>
2025-02-21 11:14:09 +0100

    hw_6_v2

--- homework-solutions/js/hw-6/task-3.js
+++ homework-solutions/js/hw-6/task-3.js
@@ -6,12 +6,15 @@
 
   Присвойте результат в переменную "unique"
 */
-let unique = [];
+let unique;
 const arrayWithDuplicate = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
-for (let i = 0; i < arrayWithDuplicate.length; i++) {
-  if (arrayWithDuplicate.includes(arrayWithDuplicate[i]) && !unique.includes(arrayWithDuplicate[i])) {
-    unique.push(arrayWithDuplicate[i]);
+let i = 0;
+while (i < arrayWithDuplicate.length) {
+  if (arrayWithDuplicate.indexOf(arrayWithDuplicate[i]) !== i) {
+    arrayWithDuplicate.splice(i, 1);
+  } else {
+    i++;
   }
 }
-
+unique = [...arrayWithDuplicate];
 export { unique };
