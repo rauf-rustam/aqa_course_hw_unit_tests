ba3b6b6c73ea89b14abb2ab9f90acec38adf6356
Rauf Rustam <raufonex@gmail.com>
2025-02-26 16:16:16 +0100

    hw_7

--- homework-solutions/js/hw-7/task-2.mjs
+++ homework-solutions/js/hw-7/task-2.mjs
@@ -3,8 +3,17 @@
  Написать функцию, которая принимает на вход слово и проверяет, является ли это слово палиндромом
 */
 
+import { fibonacci } from './task-1.mjs';
+
 function isPalindrom(word) {
   // Ваш код
+  let palindrome = false;
+  if (typeof word !== 'string') {
+    return false;
+  } else {
+    let reversWord = word.split('').reverse().join('').toLowerCase();
+    return reversWord === word.toLowerCase() && typeof word === 'string' ? true : false;
+  }
 }
 
 /*
@@ -16,6 +25,22 @@ function isPalindrom(word) {
 
 function findLongestWords(sentence) {
   // Ваш код
+  if (typeof sentence !== 'string' || sentence.length === 0) {
+    return [];
+  } else {
+    let words = sentence.split(' ');
+    let maxLenght = 0;
+    let longestWord = [];
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].length > maxLenght) {
+        maxLenght = words[i].length;
+        longestWord = [words[i]];
+      } else if (words[i].length === maxLenght) {
+        longestWord.push(words[i]);
+      }
+    }
+    return longestWord;
+  }
 }
 
 export { isPalindrom, findLongestWords };
